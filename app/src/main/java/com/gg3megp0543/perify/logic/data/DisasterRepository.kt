ff997785e0767e3a765ebeea3dc6d2ccfa99ecbe
tsukifell <calvinsaputra217@gmail.com>
2023-07-18 19:48:58 +0700

    Initial commit

---
+++ app/src/main/java/com/gg3megp0543/perify/logic/data/DisasterRepository.kt
@@ -0,0 +1,4 @@
+package com.gg3megp0543.perify.logic.data
+
+class DisasterRepository {
+}
---
+++ app/src/main/java/com/gg3megp0543/perify/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gg3megp0543.perify.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gg3megp0543.perify.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
