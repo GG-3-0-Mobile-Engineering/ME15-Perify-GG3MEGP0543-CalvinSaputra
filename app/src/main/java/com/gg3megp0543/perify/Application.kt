6ad899c97b59fd5ccf798841dcf1c3f992edd190
tsukifell <calvinsaputra217@gmail.com>
2023-07-27 11:08:33 +0700

    Adding dark mode

--- app/src/main/java/com/gg3megp0543/perify/Application.kt
+++ app/src/main/java/com/gg3megp0543/perify/Application.kt
@@ -1,9 +1,22 @@
 package com.gg3megp0543.perify
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+import com.gg3megp0543.perify.logic.helper.DarkModeEnum
+import java.util.*
 
 class Application: Application(){
     override fun onCreate() {
         super.onCreate()
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        preferences.getString(
+            getString(R.string.pref_key_dark),
+            getString(R.string.pref_dark_auto)
+        )?.apply {
+            val mode = DarkModeEnum.valueOf(this.uppercase(Locale.US))
+            AppCompatDelegate.setDefaultNightMode(mode.value)
+        }
     }
 }
--- app/src/main/java/com/gg3megp0543/perify/ui/setting/SettingsActivity.kt
+++ app/src/main/java/com/gg3megp0543/perify/ui/setting/SettingsActivity.kt
@@ -2,6 +2,8 @@ package com.gg3megp0543.perify.ui.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.gg3megp0543.perify.R
 
@@ -22,6 +24,22 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val listPreference = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+            listPreference?.setOnPreferenceChangeListener { _, newValue ->
+                val themeMode = when (newValue) {
+                    getString(R.string.pref_dark_off) -> AppCompatDelegate.MODE_NIGHT_NO
+                    getString(R.string.pref_dark_on) -> AppCompatDelegate.MODE_NIGHT_YES
+                    else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                }
+                updateTheme(themeMode)
+            }
+        }
+
+        private fun updateTheme(mode: Int): Boolean {
+            AppCompatDelegate.setDefaultNightMode(mode)
+            requireActivity().recreate()
+            return true
         }
     }
 }
